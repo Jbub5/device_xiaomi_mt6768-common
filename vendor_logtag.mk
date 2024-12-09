acex88/device_xiaomi_mt6768-common-14

mt6768-common: props: Silence gralloc4 logspam
* Since Android 13, gralloc has been spamming the logs with:
E gralloc4: Empty SMPTE 2094-40 data

Signed-off-by: bengris32 <bengris32@protonmail.ch>
Change-Id: Ib4d44443499f1396453bd7070aa1935485b329db

vendor_logtag.mk
@@ -276,4 +276,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.MAPI-SocketConnection=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.MAPI-SocketListener=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.MAPI-CommandProcessor=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.RpAudioControl=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.RpAudioControl=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL)

Jbub5/device_xiaomi_mt6768-common

vendor_logtag.mk
@@ -254,4 +254,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.libnfc_nci=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.libPowerHal=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.mTEE=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.mTEE=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL)