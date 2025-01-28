2594d801d62d9512c449b6309e72d2be024d37e4
Ksawlii <ksawery.blaszczak@proton.me>
2025-01-28 18:34:09 +0100

    mt6768-common: Migrate to Pixel power AIDL HAL

--- mt6768.mk
+++ mt6768.mk
@@ -301,7 +301,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr
+    android.hardware.power-service.pixel-libperfmgr
 
 PRODUCT_PACKAGES += \
     libmtkperf_client_vendor \
