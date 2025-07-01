xaga-risingos-devs/android_device_xiaomi_mt6895-common

mt6895-common: Silence FrameTracker logspam
Change-Id: I70f2c75c6e55b70e05bcf74358b8b661ea9d955a
Signed-off-by: ChrisCatto <chriscatto@proton.me>

vendor_logtag.mk
@@ -259,4 +259,5 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.GPUAUX=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.HWUI=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.HWUI=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.FrameTracker=$(VENDOR_LOG_LEVEL)

Jbub5/device_xiaomi_mt6768-common

configs/props/vendor_logtag.mk
@@ -289,5 +289,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.VidhanceContext=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.MDP=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.Profiler=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.GPUAUX=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.GPUAUX=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.FrameTracker=$(VENDOR_LOG_LEVEL)
 